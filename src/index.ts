b88ed799dd767a8ce1f7ca68e560a613b9713516
uglyer <root@wanda.sb>
2022-03-24 15:27:58 +0800

    重新实现触发流程, 避免死锁.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+const queueMicrotask =
+  typeof window['queueMicrotask'] == 'function'
+    ? window['queueMicrotask']
+    : window.setTimeout;
+
 /**
  * 异步功能的互斥锁
  * @author uglyer
@@ -12,13 +17,14 @@ export default class AsyncAwaitLock {
   /**
    * 等待锁数量
    */
-  protected waitLockCount = 0;
+  get waitLockCount(): number {
+    return this.waitList.length;
+  }
 
   /**
    * 等待列表
    */
-  protected waitList: Array<(target: AsyncAwaitLock) => void | Promise<void>> =
-    [];
+  protected waitList: Array<() => void> = [];
 
   /**
    * 状态锁
@@ -30,47 +36,27 @@ export default class AsyncAwaitLock {
    * 是否锁定
    */
   get isLock() {
-    return this.lockCount + this.waitLockCount >= this.maxCount;
+    return this.lockCount >= this.maxCount;
   }
 
   /**
    * 等待锁结束
+   * @deprecated {@link acquire}
    */
-  wait(): Promise<AsyncAwaitLock> {
-    return new Promise<AsyncAwaitLock>((resolve) => {
-      if (!this.isLock) {
-        resolve(this);
-        return;
-      }
-      this.waitList.push(resolve);
-    });
-  }
-
-  /**
-   * 等待锁,内部调用,上锁前校验
-   */
-  protected waitLock(): Promise<AsyncAwaitLock> {
-    return new Promise<AsyncAwaitLock>((resolve) => {
-      if (
-        this.lockCount < this.maxCount &&
-        this.waitLockCount <= this.maxCount
-      ) {
-        // 确保上锁动作同步执行
-        this.lockCount++;
-        resolve(this);
-        return;
-      }
-      this.waitList.push(resolve);
-    });
+  wait(): Promise<void> {
+    return this.acquire();
   }
 
   /**
    * 上锁 锁定前必须等待上一个锁结束
    */
   acquire() {
-    this.waitLockCount++;
-    return this.waitLock().then(() => {
-      this.waitLockCount--;
+    if (this.lockCount < this.lockCount) {
+      this.lockCount++;
+      return Promise.resolve();
+    }
+    return new Promise<void>((resolve) => {
+      this.waitList.push(resolve);
     });
   }
 
@@ -78,23 +64,19 @@ export default class AsyncAwaitLock {
    * 解锁
    */
   unlock() {
-    if (this.lockCount > 0) {
-      this.lockCount--;
+    this.lockCount--;
+    const task = this.waitList.shift();
+    if (task) {
+      this.lockCount++;
+      queueMicrotask(() => task());
     }
-    this.triggerWaitEvent();
-  }
-
-  /**
-   * 触发等待事件
-   */
-  protected async triggerWaitEvent() {
-    const event = this.waitList.shift();
-    if (!event) {
-      return;
-    }
-    await event(this);
-    if (this.lockCount < this.maxCount && this.waitLockCount <= this.maxCount) {
-      await this.triggerWaitEvent();
+    while (this.lockCount < this.maxCount) {
+      this.lockCount++;
+      const task = this.waitList.shift();
+      if (task) {
+        this.lockCount++;
+        queueMicrotask(() => task());
+      }
     }
   }
 }
